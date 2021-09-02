c3e8606281f09293fc005977d31bea6ebdc42a1d
AliAkhtarshena <89968743+AliAkhtarshena@users.noreply.github.com>
2021-09-02 15:33:59 +0430

    Add files via upload

---
+++ ali.t4.py
@@ -0,0 +1,112 @@
+# def g(x):
+#     if x >= 0:
+#         return x
+#     else:
+#         return -x
+
+
+# y = int(input())
+# z = g(y)
+# print(z)
+
+
+
+
+
+
+
+
+
+# def printer(s1 , s2 , a1 , a2):
+#     print('the deference ages of' , s1 , 'and', s2 ,'is' , g(a1-a2))
+
+
+# def g(x):
+#     if x >= 0:
+#         return x
+#     else:
+#         return -x
+
+
+
+# while True:
+#     name1 = input('what is the first name?')
+#     age1 = input('what is his age?')
+#     name2 = input('what is the first name?')
+#     age2 = input('what is his age?')
+#     printer(name1 , name2 , age1 , age2)
+
+
+
+
+
+
+
+
+
+
+# def test_function(l):
+#     l[1] = -2
+
+
+# x = [2 , 1 , 0]
+# y = x.copy()
+# test_function(x)
+# print(x)
+
+
+
+
+
+
+
+
+
+
+# x = 5
+# def g():
+#     x = 0
+# g()
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n = int(input())
+# avamel = []
+# avalha = [] 
+
+# def maghsoom_elayh(n , l):
+#     for i in range(1 , n + 1):
+#         if n % i == 0:
+#             l.append(i)
+
+# def aval_budan():(n , avalha , avamel):
+#     for x in avamel:
+#         m = 0
+#         for i in range(1 , x):
+#             if x % i == 0:
+#                 m += 1
+#             if m == 2:
+#                 avalha.append(x)
+
+
+# maghsoom_elayh(n , avamel)
+# aval_budan(n , avalha , avamel)
+# print(avalha)
